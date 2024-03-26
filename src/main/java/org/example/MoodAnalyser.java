a7d9da79e80f8e9e8095b4de4b7d9dd5dfd31f22
Vedant Jain <162889264+veduji16@users.noreply.github.com>
2024-03-26 13:43:32 +0530

    Given a Message, ability to analyse and respond Happy or Sad Mood

--- src/main/java/org/example/MoodAnalyser.java
+++ src/main/java/org/example/MoodAnalyser.java
@@ -3,7 +3,7 @@ public class MoodAnalyser {
     public String analyseMood(String mood) {
         if (mood.contains("sad")) {
             return "SAD";
-        } else if (mood.contains("happy")) {
+        } else if (mood.contains("any")) {
             return "HAPPY";
         }
         return "";
